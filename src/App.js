7d41b25dca72e7d44b152d06a2c3ca98b81744d4
Anastasiia Yarmilko <anastasiia.yarm@gmail.com>
2021-06-17 00:34:08 +0300

    Creates home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Suspense, lazy } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
+import routes from './route.js';
+import AppBar from './components/AppBar/AppBar';
 
 const HomePage = lazy(() =>
   import('./pages/HomePage/index'),
+);
+
+const MoviesPage = lazy(() =>
+  import('./pages/MoviesPage/MoviesPage'),
 );
 
 const MovieDetailsPage = lazy(() =>
@@ -10,12 +16,18 @@ const MovieDetailsPage = lazy(() =>
     './pages/MovieDetailsPage/MovieDetailsPage'),
 );
 
-const MoviesPage = lazy(() =>
-  import('./pages/MoviesPage/MoviesPage'),
-);
-
 const App = () => (
   <>
+   <AppBar />
+    <Suspense fallback={<h2>Loading...</h2>}>
+      <Switch>
+        <Route exact path={routes.home} component={HomePage} />
+        <Route path={routes.movieDetails} component={MovieDetailsPage} />
+        <Route path={routes.movies} component={MoviesPage} />
+
+        <Redirect to="/" />
+      </Switch>
+    </Suspense>
   </>
 );
 
